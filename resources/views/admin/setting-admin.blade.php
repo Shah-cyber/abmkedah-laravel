78864341ddad18acd17a21c42722babd4657701a
IzzahSN <izzahfsn@gmail.com>
2024-12-22 18:21:00 +0800

    setting-users-done

--- resources/views/admin/setting-admin.blade.php
+++ resources/views/admin/setting-admin.blade.php
@@ -1,3 +1,7 @@
 <x-admin-layout>
-    <h1>Hello from the Admin Setting Page</h1>
+    <div class="mb-4">
+        <h1 class="text-2xl font-bold text-gray-800">Admin Settings</h1>
+        <!-- Horizontal Line -->
+        <hr class="border-gray-300 my-2">
+    </div>
 </x-admin-layout>
