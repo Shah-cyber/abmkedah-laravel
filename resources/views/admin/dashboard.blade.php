8696702fa0f54d51b9a578a2173053168aa0ed3c
IzzahSN <izzahfsn@gmail.com>
2024-12-22 17:16:41 +0800

    fee-payment-list-done

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,3 +1,9 @@
 <x-admin-layout>
-    <h1>Hello from the Admin Dashboard Page</h1>
+    <!-- Header Section -->
+    <div class="mb-4">
+        <h1 class="text-2xl font-bold text-gray-800">Dashboard</h1>
+        <span class="text-sm text-gray-400">Welcome, Admin!</span>
+        <!-- Horizontal Line -->
+        <!-- <hr class="border-gray-300 my-2"> -->
+    </div>
 </x-admin-layout>
