736dcf06990eb044d321f15c35c1aabf3446ffd4
ishak138 <a.i.wildan13823@gmail.com>
2024-12-28 15:17:12 +0800

    updated event description

--- resources/views/member/event-registration.blade.php
+++ resources/views/member/event-registration.blade.php
@@ -105,78 +105,80 @@
             <div class="w-full items-center content-around">
                 <h1 class="mx-4 my-2  text-2xl font-bold text-gray-800">Personal information</h1>
                 <hr class="border-gray-300 my-2">
-                <!-- first row -->
-                <div class="flex place-content-center justify-evenly">
-                    <!-- username -->
-                    <div class="border-2 border-gray-400 w-3/5 content-around items-center flex-row flex rounded-lg m-2">
-                        <div class="flex bg-gray-300 h-full w-1/6 items-center justify-center rounded-l-md">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-9">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
-                            </svg>
+                <form method="post" action="" class="">
+                    <!-- first row -->
+                    <div class="flex place-content-center justify-evenly">
+                        <!-- username -->
+                        <div class="border-2 border-gray-400 w-3/5 content-around items-center flex-row flex rounded-lg m-2">
+                            <div class="flex bg-gray-300 h-full w-1/6 items-center justify-center rounded-l-md">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-9">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                                </svg>
+                            </div>
+                            <div class="p-2 w-full border-l-2 border-gray-400 ">
+                                <label class="block text-sm font-medium text-gray-500" for="username">
+                                    Username
+                                </label>
+                                <input class="w-full" type="text" id="username" name="username"    placeholder="Username">
+                            </div>
                         </div>
-                        <div class="p-2 w-full border-l-2 border-gray-400 ">
-                            <label class="block text-sm font-medium text-gray-500" for="username">
-                                Username
-                            </label>
-                            <input class="w-full" type="text" id="username" name="username"    placeholder="Username">
+
+                        <!-- phone number -->
+                        <div class="border-2 border-gray-400 w-3/5 content-around items-center flex-row flex rounded-lg m-2">
+                            <div class="flex bg-gray-300 h-full w-1/6 items-center justify-center rounded-l-md">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-9">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M10.5 1.5H8.25A2.25 2.25 0 0 0 6 3.75v16.5a2.25 2.25 0 0 0 2.25 2.25h7.5A2.25 2.25 0 0 0 18 20.25V3.75a2.25 2.25 0 0 0-2.25-2.25H13.5m-3 0V3h3V1.5m-3 0h3m-3 18.75h3" />
+                                </svg>
+                            </div>
+                            <div class="p-2 w-full border-l-2 border-gray-400">
+                                <label class="block text-sm font-medium text-gray-500" for="username">
+                                    Phone Number
+                                </label>
+                                <input class="w-full" type="tel" id="PhoneNumber" name="Phone Number" placeholder="Phone Number" pattern="[0-9]{10,15}">
+                            </div>
+                            
                         </div>
                     </div>
 
-                    <!-- phone number -->
-                    <div class="border-2 border-gray-400 w-3/5 content-around items-center flex-row flex rounded-lg m-2">
-                        <div class="flex bg-gray-300 h-full w-1/6 items-center justify-center rounded-l-md">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-9">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M10.5 1.5H8.25A2.25 2.25 0 0 0 6 3.75v16.5a2.25 2.25 0 0 0 2.25 2.25h7.5A2.25 2.25 0 0 0 18 20.25V3.75a2.25 2.25 0 0 0-2.25-2.25H13.5m-3 0V3h3V1.5m-3 0h3m-3 18.75h3" />
-                            </svg>
+                    <!-- second row -->
+                    <div class="flex place-content-center justify-evenly ">
+                        <!-- identity card -->
+                        <div class="border-2 border-gray-400 w-3/5 content-around items-center flex-row flex rounded-lg m-2">
+                            <div class="flex bg-gray-300 h-full w-1/6 items-center justify-center rounded-l-md">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-9">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M15 9h3.75M15 12h3.75M15 15h3.75M4.5 19.5h15a2.25 2.25 0 0 0 2.25-2.25V6.75A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25v10.5A2.25 2.25 0 0 0 4.5 19.5Zm6-10.125a1.875 1.875 0 1 1-3.75 0 1.875 1.875 0 0 1 3.75 0Zm1.294 6.336a6.721 6.721 0 0 1-3.17.789 6.721 6.721 0 0 1-3.168-.789 3.376 3.376 0 0 1 6.338 0Z" />
+                                </svg>
+                            </div>
+                            <div class="p-2 w-full border-l-2 border-gray-400">
+                                <label class="block text-sm font-medium text-gray-500" for="username">
+                                    Identity Card
+                                </label>
+                                <input class="w-full" type="number" id="IC number" name="IC number" placeholder="Identity Card">
+                            </div>  
                         </div>
-                        <div class="p-2 w-full border-l-2 border-gray-400">
-                            <label class="block text-sm font-medium text-gray-500" for="username">
-                                Phone Number
-                            </label>
-                            <input class="w-full" type="tel" id="PhoneNumber" name="Phone Number" placeholder="Phone Number" pattern="[0-9]{10,15}">
-                        </div>
-                        
-                    </div>
-                </div>
 
-                <!-- second row -->
-                <div class="flex place-content-center justify-evenly ">
-                    <!-- identity card -->
-                    <div class="border-2 border-gray-400 w-3/5 content-around items-center flex-row flex rounded-lg m-2">
-                        <div class="flex bg-gray-300 h-full w-1/6 items-center justify-center rounded-l-md">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-9">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M15 9h3.75M15 12h3.75M15 15h3.75M4.5 19.5h15a2.25 2.25 0 0 0 2.25-2.25V6.75A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25v10.5A2.25 2.25 0 0 0 4.5 19.5Zm6-10.125a1.875 1.875 0 1 1-3.75 0 1.875 1.875 0 0 1 3.75 0Zm1.294 6.336a6.721 6.721 0 0 1-3.17.789 6.721 6.721 0 0 1-3.168-.789 3.376 3.376 0 0 1 6.338 0Z" />
-                            </svg>
-                        </div>
-                        <div class="p-2 w-full border-l-2 border-gray-400">
-                            <label class="block text-sm font-medium text-gray-500" for="username">
-                                Identity Card
-                            </label>
-                            <input class="w-full" type="number" id="IC number" name="IC number" placeholder="Identity Card">
-                        </div>  
-                    </div>
-
-                    <!-- email -->
-                    <div class="border-2 border-gray-400 w-3/5 content-around items-center flex-row flex rounded-lg m-2">
-                        <div class="flex bg-gray-300 h-full w-1/6 items-center justify-center rounded-l-md">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-9">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25m19.5 0v.243a2.25 2.25 0 0 1-1.07 1.916l-7.5 4.615a2.25 2.25 0 0 1-2.36 0L3.32 8.91a2.25 2.25 0 0 1-1.07-1.916V6.75" />
-                            </svg>
-                        </div>
-                        <div class="p-2 w-full border-l-2 border-gray-400">
-                            <label class="block text-sm font-medium text-gray-500" for="username">
-                                Email
-                            </label>
-                            <input class="w-full" type="email" id="email" name="email" placeholder="email" >
+                        <!-- email -->
+                        <div class="border-2 border-gray-400 w-3/5 content-around items-center flex-row flex rounded-lg m-2">
+                            <div class="flex bg-gray-300 h-full w-1/6 items-center justify-center rounded-l-md">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-9">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25m19.5 0v.243a2.25 2.25 0 0 1-1.07 1.916l-7.5 4.615a2.25 2.25 0 0 1-2.36 0L3.32 8.91a2.25 2.25 0 0 1-1.07-1.916V6.75" />
+                                </svg>
+                            </div>
+                            <div class="p-2 w-full border-l-2 border-gray-400">
+                                <label class="block text-sm font-medium text-gray-500" for="username">
+                                    Email
+                                </label>
+                                <input class="w-full" type="email" id="email" name="email" placeholder="email" >
+                            </div>
                         </div>
                     </div>
-                </div>
 
-                <div class="m-2 p-2 flex w-full border-black justify-center place-content-center">
-                    <button type="submit" class="text-white rounded-lg bg-black hover:bg-green-600 py-3 px-6">
-                        Register
-                    </button>
-                </div>
+                    <div class="m-2 p-2 flex w-full border-black justify-center place-content-center">
+                        <button type="submit" class="text-white rounded-lg bg-black hover:bg-green-600 py-3 px-6">
+                            Register
+                        </button>
+                    </div>
+                </form>
             </div>                
         </div>
     </div>
